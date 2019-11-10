e7ca1c25e5b7d47cfc4748151aa9d6db10c2d563
D3N5 <grosdenis@gmail.com>
2019-11-10 13:17:38 +0100

    Create bootcamp POST

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 /**
  * @desc      Get all bootcamps
  * @route     GET /api/v1/bootcamps
@@ -23,8 +25,20 @@ exports.getBootCamp = (req, res, next) => {
  * @route     POST /api/v1/bootcamps
  * @access    Private
  */
-exports.createBootCamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamps' });
+exports.createBootCamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
 
 /**
